e686b61f0296f38660af9eafc5059b3c5fe2b421
civetcat <xiaojiu04032222@gmail.com>
2024-08-21 22:41:45 +0800

    Site updated: 2024-08-21 22:41:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/13/hello-world/","2024/08/13/学生管理系统的需求文档/"];function toRandomPost(){
+var posts=["2024/08/21/day01-Java基础语法/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
