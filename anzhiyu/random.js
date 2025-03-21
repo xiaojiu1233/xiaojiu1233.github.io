537cb14095c80bd6f402d7c584699b2eddca7837
Civetcat <civetcat@MacBook-Pro.local>
2025-03-21 14:19:31 +0800

    Site updated: 2025-03-21 14:19:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/18/es和kibana安装与配置/","2025/03/05/Start/","2025/02/22/Swagger/","2025/03/05/URL/","2025/02/26/docker/","2025/03/21/flex弹性布局/","2025/03/07/git/","2025/03/11/java/","2025/03/07/vue问题汇总/"];function toRandomPost(){
+var posts=["2025/03/18/es和kibana安装与配置/","2025/02/22/Swagger/","2025/02/26/docker/","2025/03/21/flex弹性布局/","2025/03/07/git/","2025/03/11/java/","2025/03/07/vue问题汇总/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
